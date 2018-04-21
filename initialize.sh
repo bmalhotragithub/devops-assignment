274fd63cd9af4f3df75864471770fdcca8a9591e
root <root@ip-10-134-81-241.us-east-2.compute.internal>
2018-04-21 09:12:04 +0000

    delete json

--- initialize.sh
+++ initialize.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-chmod +x /home/ec2-user/gitrepo/devops-assignment/playbook.yaml
-touch /home/ec2-user/gitrepo/devops-assignment/host
-echo localhost >> /home/ec2-user/gitrepo/devops-assignment/host
-ansible-playbook -i localhost /home/ec2-user/gitrepo/devops-assignment/playbook.yaml
+chmod +x /devops-assignment/playbook.yml
+ansible-playbook /devops-assignment/playbook.yml
 
